a8f9ee9aa114ac612e04d9d12b84e67e98da89b7
Sourav45-source <souravghosh7821@gmail.com>
2025-07-03 13:03:41 +0530

    Fix: ensure punkt tokenizer downloads properly

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt',quiet=True)
-nltk.download('stopwords',quiet=True)
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 ps = PorterStemmer()
